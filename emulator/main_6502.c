30108ea52c8d846e311a2b9e15e23f58ff5d48cd
Dark-Folt <marigo9794@gmail.com>
2022-10-24 11:11:16 +0200

    added comment

--- emulator/main_6502.c
+++ emulator/main_6502.c
@@ -2,21 +2,34 @@
 #include <stdint.h> 
 
 
+
+// l'ensemble des instructions
+#define INS_LDA_IM  0xA9
+
+#define MEM_SIZE  1024 * 64
+
 typedef uint8_t     byte;
 typedef uint16_t    word;
 typedef uint32_t    qword;
 
-
-
-#define INS_LDA_IM  0xA9
-
-#define MEM_SIZE  1024 * 64
+/**
+ * mem_6502
+ * Dénie la mémoire
+*/
 typedef struct
 {
     byte data[MEM_SIZE];
 
 } mem_6502;
 
+/**
+ * cpu_6502
+ * Définie un le cpu 6502
+ * avec les registres suivants: p, n, c, v, z, i
+ * program counter   : pc
+ * stack pointer    : sp
+ * registre géneraux : a, x, y 
+*/
 typedef struct
 {
     // programm counter
@@ -34,9 +47,12 @@ typedef struct
     byte d : 1;
     byte i : 1;
 
-    // void (*reset)(struct cpu_6502*)
 } cpu_6502 ;
 
+/**
+ * @param mem_6502
+ * Permet d'initialiser la mémoire
+*/
 void
 mem_initialise(mem_6502 *mem)
 {
@@ -46,6 +62,13 @@ mem_initialise(mem_6502 *mem)
     }
 }
 
+/**
+ * @param cpu_6502
+ * @param mem_6502
+ * Permet reset le cpu
+ * Reset le cpu, c'est mettre tout les registres
+ * à leurs états de départ
+*/
 void
 cpu_reset(cpu_6502 * cpu, mem_6502 *mem)
 {
@@ -68,6 +91,13 @@ cpu_reset(cpu_6502 * cpu, mem_6502 *mem)
     mem_initialise(mem);
 }
 
+/**
+ * Recupere un byte dans la mémoire
+ * @param uint32_t cycles : nombre de cycle pour le fetch
+ * @param mem_6502
+ * @param cpu_6502
+ * @return byte
+*/
 byte
 cpu_fectch_byte(uint32_t *cycles, mem_6502 *memory, cpu_6502 *cpu)
 {
@@ -78,7 +108,13 @@ cpu_fectch_byte(uint32_t *cycles, mem_6502 *memory, cpu_6502 *cpu)
     return data;
 }
 
-
+/**
+ * Permet l'execution d'un instruction qui sera
+ * lu dans la mémoire
+ * @param cycles
+ * @param memory
+ * @param cpu
+*/
 void
 cpu_execute_inst(uint32_t cycles, mem_6502 *memory, cpu_6502 *cpu)
 {
@@ -104,12 +140,17 @@ cpu_execute_inst(uint32_t cycles, mem_6502 *memory, cpu_6502 *cpu)
     
 }
 
+/**
+ * Permet d'afficher le contenue da la mémoire en hexa
+ * Parcour toute la mémoire et affiche octet par octet
+ * @param mem_6502
+*/
 void
 mem_dump(mem_6502 *mem)
 {
     size_t i;
-    for(i=0; i < MEM_SIZE; i++) {
-        printf("%02X", mem->data[i]);
+    for(i=0; i < MEM_SIZE - 1; i++) {
+        printf("%X ", mem->data[i]);
     }
     printf("\n");
 }
@@ -122,7 +163,9 @@ int main()
     cpu_reset(&cpu, &mem);
     mem.data[0xFFFC] = INS_LDA_IM;
     mem.data[0xFFFD] = 0x42;
-    cpu_execute_inst(2, &mem, &cpu);
+
+    mem_dump(&mem);
+    // cpu_execute_inst(2, &mem, &cpu);
     // int a = 1;
     // size_t o = cpu_fectch_byte(&a, &mem, &cpu);
 
